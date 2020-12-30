d38075f7bde443a4a123763d66e5caf011435c5a
Matheus <matheusvitor8143@gmail.com>
2020-12-29 23:34:37 -0300

    emproducao

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_fooddeliveryui/data/data.dart';
+import 'package:flutter_fooddeliveryui/models/restaurant.dart';
 import 'package:flutter_fooddeliveryui/widgets/recent_orders.dart';
+import 'package:flutter_fooddeliveryui/widgets/rating_stars.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -8,6 +10,82 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  _buildRestaurants(){
+    List<Widget> restaurantList = [];
+    restaurants.forEach((Restaurant restaurant ) {
+      restaurantList.add(
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15.0),
+            border: Border.all(
+              width: 1.0,
+              color: Colors.grey[200],
+            ),
+          ),
+          child:Expanded(
+            child:  Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(15.0),
+                  child: Image(
+                    height: 150.0,
+                    width: 150.0,
+                    image: AssetImage(restaurant.imageUrl),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              Expanded(
+                  child:  Container(
+                    margin: EdgeInsets.all(12.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          restaurant.name,
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+
+                        RatingStars(restaurant.rating),
+
+                        SizedBox(height: 4.0),
+                        Text(
+                          restaurant.address,
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(height: 4.0),
+                        Text(
+                          '0.2 miles away',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
+                    ),
+                  ),
+              ),
+              ],
+            ),
+          ),
+        ),
+      );
+    });
+    return Column(children: restaurantList);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -63,6 +141,23 @@ class _HomeScreenState extends State<HomeScreen> {
            ),
            ),
           RecentOrders(),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+             Padding(
+               padding: EdgeInsets.symmetric(horizontal: 20.0),
+               child:  Text(
+                 'Nearby Restaurants',
+                 style: TextStyle(
+                   fontWeight: FontWeight.w600,
+                   fontSize: 24.0,
+                   letterSpacing: 1.2,
+                 ) ,
+               ),
+             ),
+              _buildRestaurants(),
+            ],
+          )
         ],
       ),
     );
